b62dee35660880c2e1baaae4e5d1492fe9e921e1
John Mosesman <johnmosesman@gmail.com>
2022-07-26 16:53:44 -0500

    Working meta transactions

--- contracts/KudoCardSeason0.sol
+++ contracts/KudoCardSeason0.sol
@@ -1,21 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+
+import "hardhat/console.sol";
 
 /// @custom:security-contact security@kudo.app
-contract KudoCardSeason0 is ERC721, ERC721URIStorage, Pausable, AccessControl {
+contract KudoCardSeason0 is
+    ERC721,
+    ERC721URIStorage,
+    ERC2771Context,
+    Pausable,
+    AccessControl
+{
     using Counters for Counters.Counter;
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("KUDO Card Season 0", "KUDO") {
+    constructor(address trustedForwarder)
+        ERC721("KUDO Card Season 0", "KUDO")
+        ERC2771Context(trustedForwarder)
+    {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
@@ -37,6 +49,12 @@ contract KudoCardSeason0 is ERC721, ERC721URIStorage, Pausable, AccessControl {
         public
         onlyRole(MINTER_ROLE)
     {
+        // console.log("_msgData");
+        // console.logBytes(_msgData());
+
+        // console.log("msgSender");
+        // console.log(_msgSender());
+
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -50,6 +68,21 @@ contract KudoCardSeason0 is ERC721, ERC721URIStorage, Pausable, AccessControl {
     ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId);
     }
+
+    // todo: for debugging remove
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override(ERC721) {
+        console.log("transferFrom()");
+        console.log("_msgSender()", _msgSender());
+        console.logBytes(_msgData());
+
+        ERC721.transferFrom(from, to, tokenId);
+    }
+
+    // todo: for debugging remove
 
     // The following functions are overrides required by Solidity.
 
@@ -76,5 +109,25 @@ contract KudoCardSeason0 is ERC721, ERC721URIStorage, Pausable, AccessControl {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    // Overrides for ERC2771Context for meta transactions
+
+    function _msgSender()
+        internal
+        view
+        override(ERC2771Context, Context)
+        returns (address sender)
+    {
+        return ERC2771Context._msgSender();
+    }
+
+    function _msgData()
+        internal
+        view
+        override(ERC2771Context, Context)
+        returns (bytes calldata)
+    {
+        return ERC2771Context._msgData();
     }
 }
---
+++ test/KudoCardSeason0-test.ts
@@ -0,0 +1,81 @@
+import {
+  TransactionReceipt,
+  TransactionRequest,
+} from "@ethersproject/providers";
+import { expect } from "chai";
+import { Interface } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+describe("KudoCardSeason0", function () {
+  describe("meta transactions", async () => {
+    it("works", async () => {
+      const [deployer, forwarder, user1, user2] = await ethers.getSigners();
+
+      console.log("deployer", deployer.address);
+      console.log("forwarder", forwarder.address);
+      console.log("user1", user1.address);
+      console.log("user2", user2.address);
+
+      const KudoCardSeason0 = await ethers.getContractFactory(
+        "KudoCardSeason0"
+      );
+      const contract = await KudoCardSeason0.connect(deployer).deploy(
+        forwarder.address
+      );
+      await contract.deployed();
+
+      // Give user1 the NFT
+      await contract.connect(deployer).safeMint(user1.address, "some.uri");
+
+      const tokenId = 0;
+
+      // Meta-tx transfer it
+      const i: Interface = new Interface([
+        "function transferFrom(address from,address to,uint256 tokenId)",
+      ]);
+
+      const functionSignature: string = i.getSighash("transferFrom");
+
+      const txnHash: string = await forwarder.call({
+        to: contract.address,
+        data: ethers.utils.hexConcat([
+          functionSignature,
+          ethers.utils.defaultAbiCoder.encode(
+            ["address", "address", "uint256", "address"],
+            [user1.address, user2.address, tokenId, user1.address]
+          ),
+        ]),
+      });
+
+      const transactionRequest: TransactionRequest = {
+        to: contract.address,
+        data: ethers.utils.hexConcat([
+          functionSignature,
+          ethers.utils.defaultAbiCoder.encode(
+            ["address", "address", "uint256", "address"],
+            [user1.address, user2.address, tokenId, user1.address]
+          ),
+        ]),
+      };
+
+      await forwarder.sendTransaction(transactionRequest);
+
+      expect(await contract.ownerOf(tokenId)).to.eq(user2.address);
+    });
+  });
+
+  // it("Should return the new greeting once it's changed", async function () {
+  //   const Greeter = await ethers.getContractFactory("Greeter");
+  //   const greeter = await Greeter.deploy("Hello, world!");
+  //   await greeter.deployed();
+
+  //   expect(await greeter.greet()).to.equal("Hello, world!");
+
+  //   const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+  //   // wait until the transaction is mined
+  //   await setGreetingTx.wait();
+
+  //   expect(await greeter.greet()).to.equal("Hola, mundo!");
+  // });
+});
