716fb48c1a437a3bbee75e305ebc408efe51a8eb
John Mosesman <johnmosesman@gmail.com>
2022-08-09 20:29:39 -0500

    Add updatable  EIP2981 into NFT contract

---
+++ test/KudoCardSeason0-royalties-test.ts
@@ -0,0 +1,60 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("KudoCardSeason0 royalties", function () {
+  let contract: Contract;
+  let admin: SignerWithAddress,
+    user1: SignerWithAddress,
+    royaltyWallet: SignerWithAddress;
+
+  const tokenId = 1;
+
+  beforeEach(async () => {
+    const [deployer, signer1, signer2] = await ethers.getSigners();
+    admin = deployer;
+    user1 = signer1;
+    royaltyWallet = signer2;
+    // user2 = signer2;
+
+    const KudoCardSeason0 = await ethers.getContractFactory("KudoCardSeason0");
+    contract = await KudoCardSeason0.connect(deployer).deploy();
+    await contract.deployed();
+
+    await contract.connect(admin).safeMint(user1.address, "token-uri-1");
+  });
+
+  it("non-admins can't update the royalty information", async () => {
+    await expect(
+      contract.connect(user1).setDefaultRoyalty(royaltyWallet.address, 1000)
+    ).to.be.revertedWith(
+      `AccessControl: account ${user1.address.toLowerCase()} is missing role ${
+        ethers.constants.AddressZero
+      }`
+    );
+  });
+
+  it("royalty can't be more than 10%", async () => {
+    await expect(
+      contract.connect(admin).setDefaultRoyalty(royaltyWallet.address, 2000)
+    ).to.be.revertedWith("Fee too high");
+  });
+
+  it("admins can set the royalty information", async () => {
+    // Default
+    let info = await contract.royaltyInfo(tokenId, 10);
+    expect(info[0]).to.eq(ethers.constants.AddressZero);
+    expect(info[1]).to.eq(BigNumber.from(0));
+
+    await expect(
+      contract.connect(admin).setDefaultRoyalty(royaltyWallet.address, 1000)
+    )
+      .to.emit(contract, "RoyaltyUpdated")
+      .withArgs(admin.address, royaltyWallet.address, 1000);
+
+    info = await contract.royaltyInfo(tokenId, 10);
+    expect(info[0]).to.eq(royaltyWallet.address);
+    expect(info[1]).to.eq(BigNumber.from(1));
+  });
+});
