02026c7e1f8928ed7f0abf533bb62440af09171d
John Mosesman <johnmosesman@gmail.com>
2022-08-11 10:43:21 -0500

    Set admin wallets on NFT on deploy

--- deploy/00_kudo_card.ts
+++ deploy/00_kudo_card.ts
@@ -1,14 +1,33 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { DeployFunction, Deployment } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
 
-  await deploy("KudoCardSeason0", {
+  const deployment: Deployment = await deploy("KudoCardSeason0", {
     from: deployer,
     args: [],
     log: true,
   });
+
+  // Set the admins
+  const KudoCardSeason0 = await ethers.getContractFactory("KudoCardSeason0");
+  const kudoCard = await KudoCardSeason0.attach(deployment.address);
+
+  console.log("Setting admin wallets");
+
+  const adminWallets = process.env.ADMIN_WALLETS?.split(",") || [];
+
+  for (let i = 0; i < adminWallets.length; ++i) {
+    console.log("Setting admin wallet", adminWallets[i]);
+
+    const role = await kudoCard.DEFAULT_ADMIN_ROLE();
+    const txn = await kudoCard.grantRole(role, adminWallets[i], {
+      gasLimit: 300_000,
+    });
+    await txn.wait();
+  }
 };
 export default func;
